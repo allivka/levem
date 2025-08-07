51df9b26263af7ec82ce3533aeb457f52c828e11
allivka <allivka1920@gmail.com>
2025-08-07 19:31:41 +0300

    added support for multiple devices monitoring at the same time + updated readme file

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-# levem
+# levem (Linux Event Monitor)
+A lightweight CLI utility to track input events (e.g., keyboard, mouse) from `/dev/input/event*` devices in real-time.
 
 ## Description:
-simple linux cli utility for events tracking from event input device
+Monitors specified input devices and prints event messages to stdout. Spawns a separate process for each device and exits if process creation fails.
+
+## Features:
+- Multi-device monitoring
+- Real-time event printing
+- Low-overhead (uses `libevdev`)
 
 ## Build:
-- To build simply run the following shell script:
+### Dependencies:
+- `pkg-config`
+- `libevdev` (install with `sudo apt install libevdev-dev` on Debian/Ubuntu or `sudo pacman -S libevdev` on arch)
+
+### Build Command:
+- This command builds and dynamically links the executable 
 ```shell
 ./build.sh
-```
-- For successful build you need pkg-config utility and libevdev c library installed on your machine(The executable is dynamically linked by default)
-
-- To use just add levem executable to any folder located in your PATH environment variable e.g:
-```shell
-mv levem /usr/bin/levem
-```
-
-## Usage:
-```shell
-levem <event device path>
 ```
--- levem.c
+++ levem.c
@@ -6,55 +6,86 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+char *usageMessage = "Usage: %s [--help | -h] <device1 path> <device2 path> <device... path>\n";
+
+int processDevice(const char *path) {
+    pid_t pid = fork();
+    
+    if (pid < 0) {
+        return pid;
+    }
+    
+    if (pid > 0) {
+        return 0;
+    }
+    
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "Failed to open %s: %s\n", path, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    struct libevdev *dev = NULL;
+    int rc = libevdev_new_from_fd(fd, &dev);
+    if (rc < 0) {
+        fprintf(stderr, "Failed to init libevdev for %s: %s\n", path, strerror(-rc));
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Successfully initialized libevdev for %s\n", path);
+    
+    struct input_event event;
+    while (1) {
+        rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_BLOCKING, &event);
+        if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
+            printf(
+                "Event captured from %s:\n\t Type: %hu(%s) Code: %hu(%s) Value: %d(%s)\n", path,
+                event.type, libevdev_event_type_get_name(event.type), event.code,
+                libevdev_event_code_get_name(event.type, event.code), event.value,
+                libevdev_event_value_get_name(event.type, event.code, event.value));
+        } else if (rc == LIBEVDEV_READ_STATUS_SYNC) {
+            fprintf(stderr, "Synchronization with %s lost, handling SYN_DROPPED\n", path);
+            while (rc == LIBEVDEV_READ_STATUS_SYNC) {
+                rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &event);
+            }
+        } else {
+            if (rc == -ENODEV) {
+                fprintf(stderr, "%s disconnected\n", path);
+                break;
+            }
+            fprintf(stderr, "Failed to get event from %s: %s\n", path, strerror(-rc));
+            break;
+        }
+    }
+
+    libevdev_free(dev);
+    close(fd);
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <device>\n", argv[0]);
-    exit(1);
-  }
-  char *device = argv[1];
-
-  int fd = open(device, O_RDONLY);
-  if (fd < 0) {
-    perror("Failed to open device");
-    exit(1);
-  }
-
-  struct libevdev *dev = NULL;
-  int rc = libevdev_new_from_fd(fd, &dev);
-  if (rc < 0) {
-    fprintf(stderr, "Failed to init libevdev: %s\n", strerror(-rc));
-    close(fd);
-    exit(1);
-  }
-
-  printf("Succesfully initialized libevdev\n");
-
-  struct input_event event;
-  while (1) {
-    rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_BLOCKING, &event);
-    if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
-      printf(
-          "Event captured: Type: %hu(%s) Code: %hu(%s) Value: %d(%s)\n",
-          event.type, libevdev_event_type_get_name(event.type), event.code,
-          libevdev_event_code_get_name(event.type, event.code), event.value,
-          libevdev_event_value_get_name(event.type, event.code, event.value));
-    } else if (rc == LIBEVDEV_READ_STATUS_SYNC) {
-      fprintf(stderr, "Synchronization lost, handling SYN_DROPPED.\n");
-      while (rc == LIBEVDEV_READ_STATUS_SYNC) {
-        rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &event);
-      }
-    } else {
-      if (rc == -ENODEV) {
-        fprintf(stderr, "Device disconnected.\n");
-        break;
-      }
-      perror("Failed to get event");
-      break;
+    if (argc < 2) {
+        fprintf(stderr, usageMessage, argv[0]);
+        exit(EXIT_FAILURE);
     }
-  }
-
-  libevdev_free(dev);
-  close(fd);
-  return 0;
+    
+    if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
+        printf(usageMessage, argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+    
+    for (int i = 1; i < argc; i++) {
+        if (processDevice(argv[i]) < 0) {
+            perror("Failed creating a process for input event monitoring");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    while (wait(NULL) > 0);
+    
+    exit(EXIT_SUCCESS);
 }
